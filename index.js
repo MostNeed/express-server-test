67c7dddc3ccfce837716c93205e0d987cb2a3a6a
Gamja3 <44pico@naver.com>
2022-04-25 13:52:48 +0900

    commit3

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-console.log("Hello World!");
+//node_modules 내 express 관련 코드를 가져온다
+var express = require('express'); 
+var app = express();
 
-var a = 3;
+// 사용자가 http://localhost:5000/hello URL주소로 접근했을때
+app.get('/hello', (req, res) => { // URL 응답 테스트
+    res.send('hello world !')
+})
+
+app.listen(5000, ()=> {//5000 포트로 서버 오픈
+    console.log('server is running on port 5000 ...');
+})
